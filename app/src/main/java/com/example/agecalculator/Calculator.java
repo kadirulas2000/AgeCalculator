6f739e60ea2662f51e55897ceabd1e55b2829ab4
kadir ulas <kadirulas2000@gmail.com>
2022-06-21 21:36:03 +0200

    First Commit for AgeCalculator

---
+++ app/src/main/java/com/example/agecalculator/Calculator.java
@@ -0,0 +1,27 @@
+package com.example.agecalculator;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+
+public class Calculator {
+
+    int year = 2021;
+    int month = 11;
+    int day = 30;
+
+    LocalDate localDate1 = LocalDate.of(2021, 11, 30);
+    LocalDate localDateNow = LocalDate.now();
+
+    Period period = Period.between(localDate1, localDateNow);
+
+    long yearInYear = ChronoUnit.YEARS.between(localDate1, localDateNow);
+
+    long monthsInYear = ChronoUnit.MONTHS.between(localDate1, localDateNow);
+
+    long weeksInYear = ChronoUnit.WEEKS.between(localDate1, localDateNow);
+
+    long daysInYear = ChronoUnit.DAYS.between(localDate1, localDateNow);
+}
+
